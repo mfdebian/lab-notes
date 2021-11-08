c1d556b648060cbb6c739c84e55ff4bfa9e2456e
Alfredo González <alfredo.arosteguy@gmail.com>
2021-11-08 18:47:33 -0300

    Added firebase for deploying

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD2fNWVl8Rz0xGlh_CvnOIY1ZbsQ6_qLg4",
+  authDomain: "alpi-lab-notes.firebaseapp.com",
+  projectId: "alpi-lab-notes",
+  storageBucket: "alpi-lab-notes.appspot.com",
+  messagingSenderId: "116902728498",
+  appId: "1:116902728498:web:3cf87eddcfd0ef3f28fd2b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
 
 createApp(App).mount('#app')
